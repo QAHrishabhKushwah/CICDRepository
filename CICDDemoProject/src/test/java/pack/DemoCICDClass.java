0aea55e91ce75125a6886a90bfbe63bfc963a129
QAHrishabhKushwah <73633760+QAHrishabhKushwah@users.noreply.github.com>
2021-05-17 23:44:27 +0530

    New changes in file

--- CICDDemoProject/src/test/java/pack/DemoCICDClass.java
+++ CICDDemoProject/src/test/java/pack/DemoCICDClass.java
@@ -4,7 +4,7 @@ public class DemoCICDClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Success3 hrishabh ksuwah");
+		System.out.println("Success3 hrishabh ksuwah checks");
 	}
 
 }
